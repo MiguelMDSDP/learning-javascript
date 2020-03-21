22a5ef1519afe1b498f22f850271b5ef25fdfa9f
Miguel dos Santos <miguelmartinsdossantos@gmail.com>
2020-03-21 17:10:57 -0300

    Lesson 07 done

--- lesson-007/ex007.js
+++ lesson-007/ex007.js
@@ -1,3 +1,29 @@
+var day = new Date()
+
+switch (day.getDay()) {
+    case 0:
+        window.alert("Today is sunday!");
+        break;
+    case 1:
+        window.alert("Today is monday!")
+        break;
+    case 2:
+        window.alert("Today is tuesday!")
+        break;
+    case 3:
+        window.alert("Today is wednesday!")
+        break;
+    case 4:
+        window.alert("Today is thursday!")
+        break;
+    case 5:
+        window.alert("Today is friday!")
+        break;
+    case 6:
+        window.alert("Today is saturday!")
+        break;
+}
+
 function verify() {
     var txtage = window.document.getElementById("text-age");
 
